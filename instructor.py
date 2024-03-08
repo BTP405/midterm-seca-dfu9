7f3b09f90ba677056ca8bfdf98d903ba993cdc59
dfu9 <158625279+dfu9@users.noreply.github.com>
2024-03-08 23:34:54 +0000

    initial commit

--- instructor.py
+++ instructor.py
@@ -25,6 +25,10 @@ class Instructor:
             name (str): The name of the instructor.
             instructor_id (int): The unique ID of the instructor.
         """
+        self.name = name
+        self.instructor_id = instructor_id
+        self.assigned_courses = []
+
         pass
 
 
@@ -38,6 +42,8 @@ class Instructor:
         Returns:
             None
         """
+        self.assigned_courses.append(course)
+
         pass
 
 
